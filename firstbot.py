6a7679ea7d7710eeadf71d27931d77be9489eae2
nancycao <nancycao717@gmail.com>
2019-07-13 13:17:37 -0700

    pennying

--- firstbot.py
+++ firstbot.py
@@ -31,22 +31,3 @@ def tradeBonds(msg):
     if order:
         print(order)
     return order
-
-'''
-def addGlobalStateData(msg):
-    if msg["type"] == "book":
-        globalMarket[(msg["symbol"], "buy")] = msg["buy"]
-        globalMarket[(msg["symbol"], "sell")] = msg["sell"]
-
-def getFairValue(symbol):
-    offers = globalMarket[(symbol, "buy")]
-    bids = globalMarket[(symbol, "sell")]
-
-    highestOffer = max(offers, key=getPrice)
-    lowestBid = min(bids, key=getPrice)
-
-    return (highestOffer + lowestBid)/2
-
-def getPrice(item):
-    return item[0]
-'''
