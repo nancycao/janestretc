01ef2a098ee745b10cd68c1011457eb2ade69ed0
nancycao <nancycao717@gmail.com>
2019-07-13 12:15:39 -0700

    push again

--- maincontrol.py
+++ maincontrol.py
@@ -55,12 +55,12 @@ def firstbot(msg):
         for sellPrice, sellSize in sellList:
             #print(sellPrice)
             #print(sellList)
-            if sellPrice <= 1000:
+            if sellPrice < 1000:
                 # buy under 1000
                 order = {"type": "add", "order_id": orderID, "symbol": "BOND", "dir": "BUY", "price": sellPrice, "size": sellSize}
                 orderID += 1
         for buyPrice, buySize in buyList:
-            if buyPrice > 1000:
+            if buyPrice => 1000:
                 # buy under 1000
                 order = {"type": "add", "order_id": orderID, "symbol": "BOND", "dir": "SELL", "price": buyPrice, "size": buySize}
                 orderID += 1
