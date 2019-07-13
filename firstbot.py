defc6790158b792c6f13fd3b0f1eaadf664d2db3
nancycao <nancycao717@gmail.com>
2019-07-13 14:01:07 -0700

    remove average fxn for now

--- firstbot.py
+++ firstbot.py
@@ -1,3 +1,5 @@
+import tradebonds
+
 orderID = 1
 
 globalMarketData = {}
@@ -6,33 +8,12 @@ def firstbotmain(msg):
     #if msg["type"] == "book":
     #    print(msg)
     # selling bonds first
-    addGlobalStateData(msg)
+    # addGlobalStateData(msg)
     if msg["type"] == "book":
         if msg["symbol"] == "BOND":
-            return tradeBonds(msg)
-        else if msg["symbol"] == "VALBZ" or msg["symbol"] == "VALE"
-            return tradeADR(msg)
-
-def tradeBonds(msg):
-    global orderID
-    order = None
-    sellList = msg["sell"]
-    buyList = msg["buy"]
-    for sellPrice, sellSize in sellList:
-        #print(sellPrice)
-        #print(sellList)
-        if sellPrice < 1000:
-            # buy under 1000
-            order = {"type": "add", "order_id": orderID, "symbol": "BOND", "dir": "BUY", "price": sellPrice-1, "size": sellSize}
-            orderID += 1
-    for buyPrice, buySize in buyList:
-        if buyPrice > 1000:
-            # buy under 1000
-            order = {"type": "add", "order_id": orderID, "symbol": "BOND", "dir": "SELL", "price": buyPrice+1, "size": buySize}
-            orderID += 1
-    if order:
-        print(order)
-    return order
+            return tradebonds.tradeBonds(msg)
+        #else if msg["symbol"] == "VALBZ" or msg["symbol"] == "VALE"
+        #    return tradeADR(msg)
 
 def tradeADR(msg):
     fairValue = getFairValue("VALBZ")
